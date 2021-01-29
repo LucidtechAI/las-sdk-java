88d23c80f71a845306d3e1646244e58da40ab3a1
Magnus Rud <mag.aars@gmail.com>
2021-01-29 08:41:12 +0100

    clean up imports

--- src/main/java/ai/lucidtech/las/sdk/ListDocumentsOptions.java
+++ src/main/java/ai/lucidtech/las/sdk/ListDocumentsOptions.java
@@ -1,24 +1,10 @@
 package ai.lucidtech.las.sdk;
 
-import java.time.Instant;
-import java.util.Base64;
-import java.util.UUID;
-
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpUriRequest;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.NameValuePair;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ListDocumentsOptions extends OptionalListResource<ListDocumentsOptions> {
